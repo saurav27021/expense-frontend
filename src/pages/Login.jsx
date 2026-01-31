05dbcdb34a1a19b7310b7acdab32e7f16a6d7212
saurav <sauravthespi@gmail.com>
2026-01-31 12:52:02 +0530

    google sso integrated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 function Login({ setUser }) {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({
@@ -96,6 +96,33 @@ function Login({ setUser }) {
     }
 
   };
+  // throw an error during login when a user who registered via Google SSO attempts to authenticate using a ausername and password . Handle this error on the UI by displaying a clear message :"please login using google sso"
+
+
+  const handleGoogleSuccess = async (authResponse) => {
+    try {
+      const body = {
+        idToken: authResponse?.credential,
+      };
+
+      const response = await axios.post('http://localhost:5001/auth/google-auth', body, { withCredentials: true });
+      setUser(response.data.user);
+      navigate("/dashboard");
+    } catch (error) {
+      console.log(error);
+      setErrors({ message: "error occured" });
+    }
+
+
+  }
+
+  const handleGoogleFaliure = (error) => {
+    console.log(error);
+    setErrors({
+      message: "something went wrong while performing google single sign-on"
+    });
+
+  }
 
   return (
     <div className="container text-center">
@@ -108,47 +135,61 @@ function Login({ setUser }) {
       {message && (
         <div className="alert alert-success">{message}</div>
       )}
+      <div className="row justify-content-center">
+        <div className="col-6 justify-content-center">
 
-      <form onSubmit={handleFormSubmit}>
-        <div className="mb-3">
-          <label>Email:</label>
-          <input
-            className="form-control"
-            type="email"
-            name="email"
-            value={formData.email}
-            placeholder="Enter email"
-            onChange={handleChange}
-          />
-          {errors.email && (
-            <div className="text-danger">{errors.email}</div>
-          )}
+          <form onSubmit={handleFormSubmit}>
+            <div className="mb-3">
+              <label>Email:</label>
+              <input
+                className="form-control"
+                type="email"
+                name="email"
+                value={formData.email}
+                placeholder="Enter email"
+                onChange={handleChange}
+              />
+              {errors.email && (
+                <div className="text-danger">{errors.email}</div>
+              )}
+            </div>
+
+            <div className="mb-3">
+              <label>Password:</label>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                value={formData.password}
+                placeholder="Enter password"
+                onChange={handleChange}
+              />
+              {errors.password && (
+                <div className="text-danger">{errors.password}</div>
+              )}
+            </div>
+
+
+            <button className="btn btn-primary w-100 mb-3" type="submit">
+              Login
+            </button>
+
+            <div className="text-center">
+              <span>Don't have an account? </span>
+              <Link to="/register">Register here</Link>
+            </div>
+          </form>
         </div>
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-6">
+          <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+            <GoogleLogin onSuccess={handleGoogleSuccess} onError={handleGoogleFaliure} />
 
-        <div className="mb-3">
-          <label>Password:</label>
-          <input
-            className="form-control"
-            type="password"
-            name="password"
-            value={formData.password}
-            placeholder="Enter password"
-            onChange={handleChange}
-          />
-          {errors.password && (
-            <div className="text-danger">{errors.password}</div>
-          )}
+          </GoogleOAuthProvider>
+
         </div>
-
-        <button className="btn btn-primary w-100 mb-3" type="submit">
-          Login
-        </button>
-
-        <div className="text-center">
-          <span>Don't have an account? </span>
-          <Link to="/register">Register here</Link>
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
